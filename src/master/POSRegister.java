04b507d598a929cb7ae726406662cbf836c5e608
mwigmore <mattwigmore@gmail.com>
2014-07-14 22:15:08 -0500

    Added 30 items to DB. Started work on POSRegister

--- src/master/POSRegister.java
+++ src/master/POSRegister.java
@@ -8,5 +8,21 @@ package master;
  * UI portion, gets user input.  May go elsewhere or be subsumed by something else.
  */
 public class POSRegister extends POSOverwatch {
-
+	
+	POSRegister(){}
+	productDatabase database = new productDatabase();
+	
+	public void startTransaction(){
+		
+	}
+	public void couponQuery(boolean choice){
+		
+	}
+	public static void itemList(Items[] cart){
+		for(int i = 0; i <cart.length; i++){
+			System.out.println(cart[i].getItemName());
+		}
+		
+	}
+	
 }
