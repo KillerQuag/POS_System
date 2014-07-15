b81a67263c4eeb1e05f1ad1a78f9846e223f26be
KillerQuag <KillerQuag@users.noreply.github.com>
2014-07-14 22:28:18 -0500

    ProductDB gets a constructor

--- src/master/productDatabase.java
+++ src/master/productDatabase.java
@@ -9,18 +9,19 @@ import master.Items;
  */
 
 public class productDatabase extends databases {
-	int numItems = 10;
-	
+	public static final int numItems = 10;
 	Items[] ia = new Items[numItems];
-	ia[0] = new Items();
-	ia[0].setItemName("Dog Biscuits");
-	ia[0].setUID(0);
-	ia[0].setPrice(2.50);
-	itemsinArray[0].setWeight(3.20);		
-	
-	Items getItemByID(int id){
-		return ia[id];
+
+	public productDatabase() {
+		
+		ia[0] = new Items();
+		ia[0].setItemName("Dog Biscuits");
+		ia[0].setUID(0);
+		ia[0].setPrice(2.50);
+		ia[0].setWeight(3.20);	
 	}
-	
+	public Items getItemByID(int id){
+		return ia[id];
+	}	
 	//itemsinArray[0].weight = 10;
 }
